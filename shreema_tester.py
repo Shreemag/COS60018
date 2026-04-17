dee73899c913ba2d926a28135f478862b6d3cec3
Shreemag <106237532@student.swin.edu.au>
2026-04-17 12:55:20 +1000

    Added silly message

--- shreema_tester.py
+++ shreema_tester.py
@@ -4,4 +4,11 @@ if name=="Shreema":
     print("Awesome Name!")
 else:
     print("Silly name!")
-    
+
+print("=====")
+i=0
+name="name"
+print(name+"is a",end="")
+for i in range(10):
+    print("silly",end="")
+    print ("name!")
